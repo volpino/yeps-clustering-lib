dea3d34b101ce3e5439785b59aea1bb257dd9814
Pollo <pollo1_91@yahoo.it>
2010-01-12 15:43:25 +0100

    Aggiungo kmedoid.py.

--- lib/kmedoid/kmedoid.py
+++ lib/kmedoid/kmedoid.py
@@ -7,6 +7,7 @@ from numpy import array, sqrt, zeros, inf,random,empty
 import scipy.stats
 import time
 import calc_dist
+import dtw_cpu
 
 class DistanceError (Exception):
 	'''Distance calculation method not supported'''
@@ -46,48 +47,38 @@ class Medoid:
 		''' it gets as input: k (number of clusters) and a matrix (mat) of time series (one time series each column and a point each raw);
 		 it gives the index of the medoids found as output '''
 
-		if self.seed==None:
+		self.medoids = zeros(k)
+		self.medoids-=1
+		self.memo= zeros((mat.shape[1],mat.shape[1]))
+		self.memo-=1
+		self.min = zeros((mat.shape[1], 2))
+                if self.seed==None:
 			random.seed(int(time.time()))
 		else:
 			random.seed(self.seed)
+
 		self.k = k
 		self.mat = mat.astype(float)
 		self.mat=self.mat.T
+                self.r = self.mat.shape[0]
 		self.l = calc_dist.Dist(self.mat,self.met, self.fast, self.radius, pu=self.pu)
-		self.l
-		self.r = self.mat.shape[0]
-		#print "dimensione matrice",self.mat.shape
-		self.medoids = zeros(k)
-		self.el_clus = zeros (k)
-		self.memo= zeros((self.r,self.r))
-		self.memo-=1
-		self.medoids-=1
-		self.min = zeros((self.r, 2))
+		
 		self.__selectmedoid()
-		#print "inizializzazione"
-		#print self.medoids
 		if self.nrip==None:
 			conf_prec=zeros(self.k)
 			cond=0
 			while  cond==0:
 				conf_prec=self.medoids.copy()
 				self.__associate()
-				#print self.min
 				self.__swap()
-				#print "conf_prec"
-				#print conf_prec
-				#print self.medoids
 				cond=1
 				for i in range (self.k):
 					if conf_prec[i]!=self.medoids[i]:
 						cond=0
 		else:
 			for i in range(self.nrip):
-				#print self.medoids
 				self.__associate()
-				#print self.min
 				self.__swap()
-				#print self.medoids
 		matpoint=[self.mat[i] for i in self.medoids]
 		self.matpoint=matpoint
 		self.min=self.min[:,0]
@@ -111,6 +102,54 @@ class Medoid:
 
 	def __associate(self):
 		''' It assignes each series to the nearest medoid '''
+                if self.pu=="GPU":
+                        self.__associate_gpu()
+                else:
+                        self.__associate_cpu()
+
+        def __associate_cpu(self):
+                ''' It assignes each series to the nearest medoid '''
+		for i in range(self.r):	
+			self.min[i,1]=inf
+			for j in range(self.k):
+				t = self.__difference(self.mat[i].copy(), self.mat[self.medoids[j]].copy())
+				if t<self.min[i,1]:
+					self.min[i,1]=t
+					self.min[i,0]=self.medoids[j]
+
+        def __difference (self, a, b):
+		''' This fuction allows the user to choose between dtw/ddtw, euclidean distance or Pearson correlation when clustering '''
+		t=0.0
+		if (self.met=="dtw") or (self.met=="ddtw"):
+			t=self.__difference_dtw (a,b)
+		elif self.met=="euclidean":
+			t=self.__difference_eucl (a,b)
+		elif self.met=="pearson":
+			t=self.__difference_pearson (a,b)
+		return t
+
+	def __difference_dtw(self, a, b):
+                ''' It returns the distance between 2 series calculated with the dtw algorithm '''
+                if self.met=="ddtw":
+                    derivative=True
+                else:
+                    derivative=False
+                temp=dtw_cpu.compute_dtw(a, b, False, False, derivative, self.fast, self.radius)
+                return temp
+
+	def __difference_eucl (self, a, b):
+		''' It returns the euclidean distance between 2 series '''
+		val = 0 
+		for i in range(self.mat.shape[1]):
+			val += (a[i] - b[i])**2
+		return val
+
+	def __difference_pearson (self, a, b):
+		''' It returns the distance between 2 series computed with the Pearson correlation '''
+		t=scipy.stats.pearsonr(a, b)
+		return t[1]
+
+        def __associate_gpu(self):
 		cont=0
 		li=[]
 		lista=[]
@@ -119,38 +158,48 @@ class Medoid:
 				li.append((i,self.medoids[j]))
 				cont+=1
 				if cont>150000:
-					#print ("SUPERATO LIMITEass!!!")
 					cont=0
-					#print "mando ",len(li)
 					temp=self.__mem(li)
 					lista.extend(temp)
 					li=[]
 
-
-		#print "NUMERO DTW associate",len(li)
 		lista.extend(self.__mem(li))
-		#print lista,li
+
 		for i in range(self.r):
 			self.min[i,1]=inf
 			for j in range(self.k):
 				t=lista[self.k*i+j]
-				#print "%f %f %f" %(li[self.k*i+j][0],li[self.k*i+j][1],t)
 				if t<self.min[i,1]:
 					self.min[i,1]=t
-					self.el_clus[j]+=1
 					self.min[i,0]=self.medoids[j]
 
-	def __difference_pearson (self, li):
-		''' It returns the distance between 2 series computed with the Pearson correlation '''
-		temp=[]
-		for i in range (len(li)):
-			tmp=scipy.stats.pearsonr(self.mat[li[i][0],:],self.mat[li[i][1],:])
-			temp.append(tmp[1])
-			#print self.mat[li[i][0],:],self.mat[li[i][1],:]
-			#print tmp[1]
-		return temp
+        def __mem (self, li):
+		temp=empty(len(li))
+		index=[]
+		for i in range(len(li)):
+			if self.memo[li[i][0],li[i][1]]!=-1:
+				temp[i]=self.memo[li[i][0],li[i][1]]
+				li[i] = -1;
+			else:
+				index.append(i)
+		while -1 in li:
+			li.remove(-1);
 
-	def __swap (self):
+		ris=self.l.compute(li)
+		
+		for i in range(len(ris)):
+			self.memo[li[i][0],li[i][1]]=ris[i]
+			temp[index[i]]=ris[i]
+
+	        return temp
+
+        def __swap (self):
+                if self.pu=="GPU":
+                        self.__swap_gpu()
+                else:
+                        self.__swap_cpu()
+
+        def __swap_gpu (self):
 		''' for each test cluster tries to change the medoid with another series and keeps the configuration with minimum cost '''
 		lista=[]
 		li=[]
@@ -166,17 +215,13 @@ class Medoid:
 							li.append((j,z))
 							cont+=1
 							if cont>150000:
-								#print ("SUPERATO LIMITEswap!!!")
 								cont=0
-								#print "mando ",len(li)
 								temp=self.__mem(li)
 								lista.extend(temp)
 								li=[]
-		#print "NUMERO DTW swap",len(li)
-		lista.extend(self.__mem(li))
-		#print "RITORNA LUNGA ",len(lista)
 
-		self.el_clus = zeros (self.k)
+		lista.extend(self.__mem(li))
+
 		p=0
 		for i in range(self.k):
 			medoid=self.medoids[i]
@@ -187,54 +232,35 @@ class Medoid:
 					p+=1
 			for j in range (self.r):
 				if (self.medoids[i]==self.min[j,0]) and (not j in self.medoids):
-					#print "provo con la time ",j
 					new_conf=0
 					for z in range (self.r):
 						if (self.min[z,0]==self.medoids[i]) and (z!=j):
 							new_conf+=lista[p]
 							p+=1
-					#print "costa ",new_conf
 					if new_conf<old_conf:
-						#print "e' meglio, scambio"
 						old_conf=new_conf
 						medoid=j
 			self.medoids[i]=medoid
-		#print self.medoids
 
-	def __cost (self,newmed, cluster):
-		''' calculates the cost of cluster's configuration '''
-#		print "chiamata a cost ",newmed,cluster
-		sum_dist=0
-		for j in range (self.r):
-			if (self.min[j,0]==cluster) and (j!=newmed):
-#				print "analizzo righe",j,newmed
-				sum_dist+=self.__difference(self.mat[j,:].copy(),self.mat[newmed,:].copy())
-		return sum_dist
-
-	def __mem (self, li):
-		temp=empty(len(li))
-		index=[]
-		for i in range(len(li)):
-			if self.memo[li[i][0],li[i][1]]!=-1:
-				temp[i]=self.memo[li[i][0],li[i][1]]
-				li[i] = -1;
-			else:
-				index.append(i)
-
-		while -1 in li:
-			li.remove(-1);
-		if self.met!="pearson":
-			ris=self.l.compute(li)
-		else:
-			ris=self.__difference_pearson(li)
-
-
-		for i in range(len(ris)):
-			self.memo[li[i][0],li[i][1]]=ris[i]
-			temp[index[i]]=ris[i]
-
-		return temp
-
+        def __swap_cpu (self):
+                for i in range(self.k):
+			medoid=self.medoids[i]
+			old_conf=0
+			for z in range (self.r):
+				if (self.min[z,0]==self.medoids[i]) and (z!=self.medoids[i]):
+					old_conf+=self.__difference(self.mat[self.medoids[i]].copy(),self.mat[z].copy())
+			for j in range (self.r):
+				if (self.medoids[i]==self.min[j,0]) and (not j in self.medoids):
+					new_conf=0
+					for z in range (self.r):
+						if (self.min[z,0]==self.medoids[i]) and (z!=j):
+							new_conf+=self.__difference(self.mat[j].copy(),self.mat[z].copy())
+					if new_conf<old_conf:
+						old_conf=new_conf
+						medoid=j
+			self.medoids[i]=medoid
+                
+		
 if __name__ == "__main__":
 	k = 2
 	mat = array(   [[0,0,0,1,1,1,2,2,2,12,12,12,13,13,13,14,14,14,0,0,0,1,1,1,2,2,2],[0,1,2,0,1,2,0,1,2,7,8,9,7,8,9,7,8,9,10,11,12,10,11,12,10,11,12]] )
