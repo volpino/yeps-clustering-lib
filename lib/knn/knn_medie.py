ee30bbcab71349377cfe2b4f009a1318849ace98
0flash0 <fabilev@libero.it>
2010-01-13 20:27:42 +0100

    knn_medie.py fixed, be aware the function needs one argument less respect to the previous version. The "label" argument sould not be passed.

--- lib/knn/knn_medie.py
+++ lib/knn/knn_medie.py
@@ -5,11 +5,10 @@ import numpy as np
 
 class kS:
 
-    def __init__(self, ts, training_set, labels, centroids,
+    def __init__(self, ts, training_set, centroids,
                  distance_type="dtw", fast=False, radius=20, pu="CPU"):
         self.ts = ts
         self.training_set = training_set
-        self.labels = labels
         self.centroids = centroids
         self.distance_type = distance_type
         self.fast = fast
@@ -18,7 +17,7 @@ class kS:
         self.pu = pu
 
         self.tmp_matrix = []
-        for i in range(len(self.training_set)):
+        for i in self.centroids:
             self.tmp_matrix.append(self.training_set[i])
         self.tmp_matrix.append(self.ts)
 
@@ -34,28 +33,15 @@ class kS:
         self.nn=[]  
         self.dist_list = []
         for i in range(len(self.dist_list_tmp)):
-            a = [self.labels[i],self.dist_list_tmp[i]]
+            a = [self.dist_list_tmp[i], self.centroids[i]]
             self.dist_list.append(a)      
         
         self.dist_list.sort()
-        self.centroids.sort()
-       	self.dist_list = zip(*self.dist_list)           
-        self.cluster_averages = []
-        for i in range(len(self.centroids)):
-            start = self.dist_list[0].index(self.centroids[i])
-            try:
-                end = self.dist_list[0].index(self.centroids[i+1])
-            except IndexError:
-                end = len(self.dist_list[0])
-            avr = sum(self.dist_list[1][start:end]) / len(self.dist_list[1][start:end])
-            self.cluster_averages.append([avr, self.centroids[i]])
-        
-        self.cluster_averages.sort()
-        self.mindist = self.cluster_averages[0][0]
+        self.mindist = self.dist_list[0][0]
         i = 0
         try:
-            while (self.cluster_averages[i][0] == self.mindist):
-                self.nn.append(self.cluster_averages[i][1])
+            while (self.dist_list[i][0] == self.mindist):
+                self.nn.append(self.dist_list[i][1])
                 i = i+1
         except IndexError:
         	pass
@@ -65,13 +51,11 @@ if __name__ == '__main__':
     ts = [1,2,3,4,5]
     training_set = [[1,2,3,4,5],[5,4,3,2,1],[5,4,4,2,1],[2,2,3,4,5]]
     centroids = [1,2,3]
-    labels = [3,2,2,1]
     distance_type = 'dtw'
     fast = False
     radius = 20
     nn = kS(ts,
              training_set,
-             labels,
              centroids,
              distance_type,
              fast,
