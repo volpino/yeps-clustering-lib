be686ed9a7a969bfec295ccbf99075e8715ec051
severinozeni <severinik91@htomail.it>
2010-01-15 16:15:21 +0100

    proper knn version

--- lib/knn/knn.py
+++ lib/knn/knn.py
@@ -5,21 +5,27 @@ import numpy as np
 
 class kNN:
 
-    def __init__(self, ts, training_set, labels, centroids,
+    def __init__(self, test_set, training_set, label_list,
                  distance_type="dtw", fast=False, radius=20, pu="CPU"):
-        self.ts = ts
+        self.test_set = test_set
         self.training_set = training_set
-        self.labels = labels
-        self.centroids = centroids
+        self.label_list = label_list
         self.distance_type = distance_type
         self.fast = fast
         self.radius = radius
+        self.labels = []
         self.calc_list = []
-
         self.tmp_matrix = []
+
+        for i in range(len(self.label_list)):
+            if self.labels.__contains__(self.label_list[i]) == False:
+                self.labels.append(self.label_list[i])
+            else:
+                pass
+
         for i in range(len(self.training_set)):
             self.tmp_matrix.append(self.training_set[i])
-        self.tmp_matrix.append(self.ts)
+        self.tmp_matrix.append(self.test_set)
 
         for i in range(len(self.tmp_matrix) - 1):
             self.calc_list.append((len(self.tmp_matrix) - 1, i))
@@ -30,54 +36,43 @@ class kNN:
                                      self.radius,
                                      pu=pu).compute(self.calc_list)
 
-    def compute(self, k=1):
-        self.k=k
-        self.label_list=[]
-        self.label_dict={}
-        self.nn=[]
-        self.dist_list=[]
+    def compute(self, k=1, weighted):
+        self.k = k
+        self.nn = []
+        self.dist_list = []
+        self.label_dict = {}
+        self.ts_label = []
 
         for i in range(len(self.dist_list_tmp)):
+        #from numpy array to list
             self.dist_list.append(self.dist_list_tmp[i])
 
-        if k==1:
-            if self.dist_list.count(min(self.dist_list))==1:
-                self.nn.append(self.labels[self.dist_list.index(min(self.dist_list))])
-            else:
-                for i in range(self.dist_list.count(min(self.dist_list))):
-                    self.nn.append(self.labels[self.dist_list.index(min(self.dist_list))])
-                    self.dist_list[self.dist_list.index(min(self.dist_list))]=np.inf
-                for i in range(len(self.nn-1)):
-                    if self.nn[j]==self.nn[j+1]:
-                        self.nn.__delitem__(j+1)
-                    else:
-                        continue
-
-        else:
-            for i in range(self.k):
-                self.label_list.append(int(self.dist_list.index(min(self.dist_list))))
+        for i in range(self.k):
+            for j in range(self.dist_list.count(min(self.dist_list))):
+                self.nn.append(self.label_list[self.dist_list.index(min(self.dist_list))])
                 self.dist_list[self.dist_list.index(min(self.dist_list))]=np.inf
-            for i in range(len(self.centroids)):
-                self.label_dict[str(self.centroids[i])] = self.dist_list.count(self.centroids[i])
-            while self.nn==[] or self.nn>max(self.label_dict, key=lambda x:self.label_dict.get(x)):
-                self.nn.append(int(max(self.label_dict, key=lambda x:self.label_dict.get(x))))
 
-        return self.nn
+        for i in range(len(self.label_list)):
+            self.label_dict[self.label_list[i]] = self.dist_list.count(self.label_list[i])
+
+
+        self.ts_label.append(max(self.label_dict, key=lambda x:self.label_dict.get(x)))
+
+
+        return self.ts_label
 
 
 if __name__ == '__main__':
-    ts = [1,2,3,4,5]
+    test_set = [1,2,3,4,5]
     training_set = [[1,2,3,4,5],[5,4,3,2,1],[5,4,4,2,1],[2,2,3,4,5]]
-    centroids = [1,2]
-    labels = [1,2,2,1]
+    labels = ['a','b','b','a']
     distance_type = 'dtw'
     fast = False
     radius = 20
-    k = 2
-    nn = kNN(ts,
+    k = 3
+    nn = kNN(test_set,
              training_set,
              labels,
-             centroids,
              distance_type,
              fast,
              radius)
